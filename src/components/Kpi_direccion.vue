ff967753346542c03958f0033b223be9560562b5
servicio-pr <servicioti2@proyectoroberto.org.mx>
2024-06-10 13:22:31 -0600

    componentes

---
+++ src/components/Kpi_direccion.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="KPI">
+      <h1>Info de las kpi</h1>
+      // Se generara un bucle para obtener la info del historial
+      // esta vista es para direccion
+      <table>
+        <thead>
+            <tr>
+                <th class="uno">
+                  <p>Aqui aparece la informacion de las kpi</p>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Usuario</td>
+                <td>Mes</td>
+                <td>Area TI</td>
+                <td>Número de ticket</td>
+                <td>Tiempo invertido</td>
+                <td>KPI</td>
+                <td>Cumple</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+              <td>6</td>
+              <td>7</td>
+          </tr>
+        </tbody>
+    </table>
+    </div>
+  </template>
+  
+  <script>
+  </script>
+  
+  <style scoped lang="scss">
+  </style>  
