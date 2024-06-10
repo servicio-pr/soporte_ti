c578888ffa2bb9e6ce9e88d71f26e4839acd5253
jose-j-hv <josejuanhv9512@gmail.com>
2024-06-09 23:32:12 -0600

    Casi todas las vistas

--- src/components/Infio_ticket.vue
+++ src/components/Infio_ticket.vue
@@ -1,29 +1,30 @@
 <template>
   <div class="Estatus_ticket">
     <h1>Seguimiento de ticket</h1>
+    // Se generara un bucle para obtener la info del historial
+    <p>Aqui aparece la informacion del ticket</p>
     <form id="nuevo"
         @submit="checkForm"
         accion ="hhtps://vuejs.org/"
         method = "post"
         >
             <p>
-                <label for="ticket">Numero de ticket </label>
+                <label for="res">Responder: </label>
                 <input
-                  id="ticket"
-                  v-model="ticket"
+                  id="res"
+                  v-model="res"
                   type="text"
-                  name="ticket"
+                  name="res"
                 >
             </p>
             <p>
-                <label for="email">Correo </label>
-                <input
-                  id="email"
-                  v-model="email"
-                  type="text"
-                  name="email"
-                  min="0">
-            </p>
+              <label for="file">Adjuntar evidencias  </label>
+              <input
+                  type="file"
+                  id="fileInput"
+                  @change="handleFileUpload"
+              >
+          </p>
             <p>
                 <input
                   type="submit"
